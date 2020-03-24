31718ce43750f64dca33bf766ec64677e55d13da
K.Kiyokawa <koichi20110068@gmail.com>
2020-03-24 17:00:44 +0900

    chore: install axios and dotenv and fontawesome

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,3 +1,6 @@
-export const getItems = () => {
-  fetch()
-}
+import axios from 'axios'
+
+export const getItems = async() => (
+  axios.get(process.env.ENDPOINT)
+    .then(response => response.data)
+)
